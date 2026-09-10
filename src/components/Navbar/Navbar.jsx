01806d70dcf99ac87d7a0539e546387c4f077425
riteshraman47 <riteshraman47@gmail.com>
2026-09-10 23:12:35 +0530

    Made Header and ExploreMenu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 const Navbar = () => {
+   const[menu,setMenu]= useState("home");
+
+
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        <li onClick={()=>setMenu("home")} className={menu==='home'?"active":""}>home</li>
+        <li onClick={()=>setMenu("menu")} className={menu==='menu'?"active":""}>menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu==='mobile-app'?"active":""}>mobile-app</li>
+        <li onClick={()=>setMenu("contact us")} className={menu==='contact us'?"active":""}>contact us</li>
       </ul>
       <div className="right-navbar">
         <img src={assets.search_icon} alt="" />
+
         <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
-          <div className="buttontag">
-            <button>sigh in</button>
-          </div>
         </div>
+        <div className="buttontag">
+            <button>sign in</button>
+          </div>
       </div>
     </div>
   )
